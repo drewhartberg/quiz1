c947f7259a747a5a01b576d0121af3e420de2c6c
Drew Hartberg <drewhartberg@gmail.com>
2020-01-20 22:15:06 +0000

    Added form

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -3,4 +3,19 @@ class WordsController < ApplicationController
   def index
     @word = Word.order("RANDOM()").first
   end
+
+  def new
+    @word = Word.new
+  end
+
+  def create
+    Word.create(quote_params)
+    redirect_to root_path
+  end
+
+  private
+
+  def quote_params
+    params.require(:word).permit(:word)
+  end
 end
